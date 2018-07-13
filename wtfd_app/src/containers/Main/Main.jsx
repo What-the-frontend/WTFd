831de2320471a8c8618ac76be5abdc4210532b68
geni429 <geni429@gmail.com>
2018-07-13 15:15:09 +0900

    [Create] Main/MainSection.jsx, show WTFd markdown success

--- wtfd_app/src/containers/Main/Main.jsx
+++ wtfd_app/src/containers/Main/Main.jsx
@@ -1,11 +1,29 @@
 import React, { Component, Fragment } from 'react';
+import axios from 'axios';
 import { HeaderNavigation } from '../../components';
+import MainSection from './MainSection';
 
 class Main extends Component {
+  state = {
+    readme: ''
+  }
+
+  componentDidMount = async () => {
+    try {
+      const response = await axios.get('https://api.github.com/repos/What-the-frontend/WTFd/readme');
+      this.setState({
+        readme: atob(response.data.content)
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   render() {
     return (
       <Fragment>
         <HeaderNavigation />
+        <MainSection readme={this.state.readme} />
       </Fragment>
     );
   }
---
+++ wtfd_app/src/containers/Main/MainSection.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+import ReactMarkdown from 'react-markdown';
+
+const Section = styled.section`
+  width: 800px;
+  margin: auto;
+`
+
+const ReadMeContent = styled.article`
+  font-size: 20px;
+  color: #7b7b7b;
+`
+
+const MainSection = ({ readme }) =>
+  <Section>
+    <ReadMeContent>
+      <ReactMarkdown source={readme}/>
+    </ReadMeContent>
+  </Section>
+
+export default MainSection;
