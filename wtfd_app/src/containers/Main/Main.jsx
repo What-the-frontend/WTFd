c752ead6d138b64a559f075c04cf316ecf782cc9
geni429 <geni429@gmail.com>
2018-07-13 12:22:25 +0900

    [Create] wtfd_app by create-react-app and initialized

---
+++ wtfd_app/src/containers/Main/Main.jsx
@@ -0,0 +1,11 @@
+import React, { Component, Fragment } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <Fragment />
+    );
+  }
+}
+
+export default Main;
---
+++ wtfd_app/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Main } from './containers';
+
+const root = document.getElementById('root');
+render(<Main />, root);
